3a0310f2e5659252e4e7caf4cc5704a7e7e72d8c
Ayekaba <ayekaba.mengue@hotmail.com>
2020-07-01 03:23:24 +0200

    Commit 01_07

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+
+    bool CanSpawn = true;
+    float minSpawnTime = 1.5f;
+    float maxSpawnTime = 6f;
+    [SerializeField] Attacker attacker;
+
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (CanSpawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Debug.Log("new enemy!");
+        Instantiate(attacker, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    float speed = 6f;
+    float damage = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var health = collision.GetComponent<Health>();
+
+        var attacker = collision.GetComponent<Attacker>();
+        if(attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+
+        }
+
+    }
+}
